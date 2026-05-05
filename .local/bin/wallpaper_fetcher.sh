9e50793593eaf899bf642e40c8a15532626d5d04
igu <igu@example.com>
2026-05-05 10:49:00 -0300

    feat: filter wallpapers by 'classical art' tag

--- .local/bin/wallpaper_fetcher.sh
+++ .local/bin/wallpaper_fetcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Fetch a random wallpaper from Wallhaven and update hyprpaper
 TARGET="$HOME/Pictures/wallpaper.jpg"
-API_URL="https://wallhaven.cc/api/v1/search?sorting=random&purity=100&categories=111"
+API_URL="https://wallhaven.cc/api/v1/search?sorting=random&purity=100&categories=111&q=id:23416"
 # Fetch the JSON response and use jq to extract the path
 IMG_URL=$(curl -s "$API_URL" | jq -r '.data[0].path')
 wget "$IMG_URL" -O "$TARGET"
