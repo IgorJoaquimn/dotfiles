4a16ad7e8efc0ac8c28cdc65df98e843538b9fc7
IgorJoaquimn <igorjoaquim.nunes@gmail.com>
2026-05-05 22:08:40 -0300

    fix(sddm): update sddm background when cycling local wallpapers

--- .local/bin/cycle_wallpaper.sh
+++ .local/bin/cycle_wallpaper.sh
@@ -25,6 +25,9 @@ echo "$next_index" > "$STATE_FILE"
 
 NEXT_WALLPAPER="${images[$next_index]}"
 
+# Update SDDM background
+cp "$NEXT_WALLPAPER" "/usr/share/sddm/themes/pixel-coffee/background.jpg"
+
 # Use swaybg to set the wallpaper
 # Kill existing swaybg processes
 killall swaybg 2>/dev/null
